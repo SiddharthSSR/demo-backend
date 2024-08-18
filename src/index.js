ee232fbc6809d4dbcf64c7b0554d73da68c5afe9
Siddharth Singh <siddharthsingh@codingtensor.local>
2024-08-18 23:49:39 +0530

    added db support

---
+++ src/index.js
@@ -0,0 +1,32 @@
+import dotenv from "dotenv/config"
+import connectDB from "./db/index.js";
+
+
+
+connectDB()
+
+/*
+* This approach has all the functions and methods inside index.js
+* Polluting the file too much
+* Shifting to folder DB for functions and then calling them from this file
+import express from "express";
+const app = express();
+
+( async () => {
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        app.on("error", (error) => {
+            console.log("ERROR:", error);
+            throw error
+        })
+
+        app.listen(process.env.PORT, () => {
+            console.log(`App is listening on port: ${process.env.PORT}`)
+        })
+
+    } catch(error) {
+        console.error("ERROR:", error);
+        throw error
+    }
+})()
+*/
